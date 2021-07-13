d9be7a32118f7fd7ff8380db031563c252897fdf
molk12 <malloukabac@gmail.com>
2021-07-13 09:41:15 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Navx from './comonents/navcomp'
+import Formx from './comonents/forms'
+
+function App() {
+  return (
+    <div className="App">
+     <h1> hello word </h1>
+     <Navx />
+     <Formx/>
+    
+    </div>
+  );
+}
+
+export default App;
